0f27864b4e21e291d499bb03134e1a1fdf3639ba
Mat.L <eric_op@163.com>
2017-03-22 15:55:55 +0800

    add new page dataCompare.php

--- application/controllers/website.php
+++ application/controllers/website.php
@@ -6,11 +6,20 @@ class Website extends MY_Controller {
         parent::__construct();
     }
 
-	public function index(){
+	public function postDataLegacyView(){
         $data = array();
         $data['pageId'] = FACEBOOK_WEBSITE_PAGEID;
         $data['pageAccessToken'] = FACEBOOK_WEBSITE_PAGE_ACCESS_TOKEN;
         $content = $this->load->view('pages/general/content/postDataLegacyView', $data, true);
+		$this->loadview($content);
+	}
+    
+	public function dataCompare(){
+        $data = array();
+        $data['websiteName'] = FACEBOOK_WEBSITE_NAME;
+        $data['pageId'] = FACEBOOK_WEBSITE_PAGEID;
+        $data['pageAccessToken'] = FACEBOOK_WEBSITE_PAGE_ACCESS_TOKEN;
+        $content = $this->load->view('pages/general/content/dataCompare', $data, true);
 		$this->loadview($content);
 	}
 }
---
+++ application/views/pages/general/content/dataCompare.php
@@ -0,0 +1,224 @@
+  <!-- Content Wrapper. Contains page content -->
+  
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Data Compare
+      </h1>
+      <ol class="breadcrumb">
+        <!--<li><a href="<?= base_url() ?>overview"><i class="fa fa-dashboard"></i> Overview</a></li>-->
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+      
+        <style>
+            .pageContainer{
+                white-space:nowrap;
+                text-align: left;
+            }
+            .pageContainer label{
+                padding-left:10px;
+            }
+            table.dataTable thead .sorting:after{
+                opacity:0;
+            }
+            table.dataTable thead .sorting:after, table.dataTable thead .sorting_asc:after, table.dataTable thead .sorting_desc:after{
+                opacity:0;
+            }
+            table.table-bordered.dataTable{
+                border-collapse: collapse !important;
+            }
+            table.table-bordered.dataTable tbody th, table.table-bordered.dataTable tbody td{
+                vertical-align: baseline;
+            }
+        </style>
+        <div class="col-md-12">
+          <div class="box box-info ptwBox">
+            <div class="box-header">
+              <h3 class="box-title text-aqua">Pages to Watch</h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <label>Compare the performance of your Page and posts with similar Pages on Facebook.</label>
+                <table id="ptwDataTable" class="table table-striped table-bordered dt-responsive" cellspacing="0" width="100%"></table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+        </div>
+        
+        <div class="col-md-6">
+          <div class="box box-info">
+            <div class="box-header">
+              <h3 class="box-title text-aqua"><?php echo $websiteName;?> Panel:</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+            </div>
+            <div class="box-body">
+                <div class="form-group">
+                  <input type="text" class="form-control" value="<?php echo $websiteName;?>" disabled="">
+                </div>
+                
+                <div class="form-group">
+                    <label>Date range:</label>
+                    <div class="input-group">
+                        <button type="button" class="btn btn-default pull-right" id="etdaterange-btn">
+                        <span>
+                          <i class="fa fa-calendar"></i> Date range picker
+                        </span>
+                        <i class="fa fa-caret-down"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title text-muted">Customize Panel:</h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+              </div>
+            </div>
+            <div class="box-body">
+                <div id="pageToWatchList" class="form-group">
+                    <select class="form-control">
+                        <option value="<?php echo $pageId;?>"><?php echo $websiteName;?></option>
+                    </select>
+                </div>
+                
+                <div class="form-group">
+                    <label>Date range:</label>
+                    <div class="input-group">
+                        <button type="button" class="btn btn-default pull-right" id="customizedaterange-btn">
+                        <span>
+                          <i class="fa fa-calendar"></i> Date range picker
+                        </span>
+                        <i class="fa fa-caret-down"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-12">
+          <!-- LINE CHART -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Number of Posts
+                <span class="nopSummaryContainer"></span>
+              </h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="chart">
+                <canvas id="lineChart_nop" style="height:300px"></canvas>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        
+        <div class="col-md-12">
+          <!-- LINE CHART -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Avg Reach By Posts
+                <span class="arSummaryContainer"></span>
+              </h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="chart">
+                <canvas id="lineChart_ar" style="height:300px"></canvas>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        
+        <div class="col-md-12">
+          <!-- LINE CHART -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Avg Page Fans Like
+                <span class="apflSummaryContainer"></span>
+              </h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="chart">
+                <canvas id="lineChart_apfl" style="height:300px"></canvas>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        
+        <div class="col-md-12">
+          <!-- LINE CHART -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Reach% (Avg Reach / Avg Fans Like)
+                <span class="rpSummaryContainer"></span>
+              </h3>
+
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="chart">
+                <canvas id="lineChart_rp" style="height:300px"></canvas>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+<script>
+    
+</script>
