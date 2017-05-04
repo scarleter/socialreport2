3f13e528725ff6b4d1db11c237b2292b2768bb13
Mat.L <eric_op@163.com>
2017-05-04 15:46:08 +0800

    add generateWeeklyReportExcel function

--- application/controllers/website.php
+++ application/controllers/website.php
@@ -47,4 +47,39 @@ class Website extends MY_Controller {
         $content = $this->load->view('pages/general/facebook/postLog', $data, true);
 		$this->loadview($content);
     }
+    
+    public function generateWeeklyReportExcel(){
+        $weeklyReportData = json_decode($_POST['weeklyReportData']);
+        $excelName = $_POST['excelName'];
+        $destinationHighestCoordinate = array('destinationHighestCol' => 'A', 'destinationHighestRow' => '1');
+        $this->load->library('ExcelTemplate', array('templatePath' => './admin/views/excelTemplate/FacebookWeeklyReportTemplate.xlsx', 'excelName' => $excelName));
+        $exceltemplate = $this->exceltemplate;
+
+        foreach($weeklyReportData as $weeklyReportDataKey => $weeklyReportDataValue){        
+            $header = $weeklyReportDataValue -> header;
+            $postLogData = $weeklyReportDataValue -> postLogData;
+            $footer = $weeklyReportDataValue -> footer;
+
+            //add 5 more rows when create a new weekly report below the previous one
+            if($weeklyReportDataKey != 0){
+                $destinationHighestCoordinate['destinationHighestRow'] += 5;
+            }
+
+            //fill header
+            $destinationHighestCoordinate = $exceltemplate->fillDataFromTplSheet('A', $destinationHighestCoordinate['destinationHighestRow'], $exceltemplate->template->getSheet(0), 'A', 1, $header);
+
+            //loop to fill postLog content
+            foreach($postLogData as $postLogDataKey => $postLogDataValue){
+                $destinationHighestCoordinate = $exceltemplate->fillDataFromTplSheet('A', $destinationHighestCoordinate['destinationHighestRow'], $exceltemplate->template->getSheet(1), 'A', 1, $postLogDataValue);
+            }
+
+            //fill footer
+            $destinationHighestCoordinate = $exceltemplate->fillDataFromTplSheet('A', $destinationHighestCoordinate['destinationHighestRow'], $exceltemplate->template->getSheet(2), 'A', 1, $footer);
+        }
+        
+        //generate excel in server
+        $exceltemplate->outputExcel();
+        
+        echo json_encode(array('downloadUrl' => $exceltemplate->downloadUrlRoot.$exceltemplate->excelName.$exceltemplate->suffix)); 
+    }
 }
